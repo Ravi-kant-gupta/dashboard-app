b1955daa3285c17b9edc20c09334decd8a1c24bc
Ravi <kantgupta1995@gmail.com>
2024-07-14 18:01:13 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
-// const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = 3307 ;
 
@@ -11,8 +9,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-// EndPoint-- project.ct80wmq0it39.ap-south-1.rds.amazonaws.com
-//- port 3306
 
 const db = mysql.createConnection({
   host: 'project.ct80wmq0it39.ap-south-1.rds.amazonaws.com',
@@ -30,53 +26,27 @@ db.connect(err => {
   }
   console.log('Connected to the database');
 
-  // Execute the create table query
-  // db.query(createTableQuery, (err, createResults) => {
-  //     if (err) {
-  //         console.error('Error creating table:', err);
-  //         db.end();
-  //         return;
-  //     }
-  //     console.log('Table created successfully:', createResults);
-      // Execute the insert data query
-      // db.query(insertQuery, (err, insertResults) => {
-      //     if (err) {
-      //         console.error('Error inserting data:', err);
-      //     } else {
-      //         console.log('Data inserted successfully:', insertResults);
-      //     }
-          // // Close the db
-          // db.end();
-      //});
-  //});
+  
 });
 
 
-
-// CRUD endpoints
-// app.get('/get', (req, res) => {
-//   db.query('SELECT * FROM dashboard_tbl', (err, results) => {
-//     if (err){ throw err};
-//     console.log(results);
-//     res.send(results);
-//   });
-// });
-
+let menuId = 0
 app.get('/api/dashboard', (req, res) => {
   db.query('SELECT * FROM dashboard_tbl', (err, results) => {
     if (err) throw err;
-    console.log(results)
+    menuId = results.length;
+    console.log(menuId)
     res.json(results);
   });
 });
 
 app.post('/api/dashboard', (req, res) => {
   const { menu_heading, menu_name, menu_under, enable_yn } = req.body;
-  const menuid = uuidv4();
   db.query('INSERT INTO dashboard_tbl (menuid, menu_heading, menu_name, menu_under, enable_yn) VALUES (?, ?, ?, ?, ?)',
-    [menuid, menu_heading, menu_name, menu_under, enable_yn], (err, results) => {
+    [menuId+1, menu_heading, menu_name, menu_under, enable_yn], (err, results) => {
       if (err) throw err;
-      res.json({ id: menuid });
+      console.log(results)
+      res.json({ id: menuId+1 });
     });
 });
 app.put('/api/dashboard/:id', (req, res) => {
@@ -97,7 +67,6 @@ app.delete('/api/dashboard/:id', (req, res) => {
   });
 });
 
-// Add other CRUD endpoints...
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
